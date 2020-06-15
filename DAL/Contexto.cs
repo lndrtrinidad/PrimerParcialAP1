2b1e10f6c0747a568c3bb8ba0179b96027f97602
LndrT <LndrT@LAPTOP-3U14ODTN>
2020-06-15 18:13:18 -0400

    add context and registro UI

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroVehiculo.DAL
+{
+    public class Contexto : DbContext
+    {
+
+    }
+}
---
+++ UI/Registro/RegistroV.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RegistroVehiculo.UI.Registro
+{
+    /// <summary>
+    /// Interaction logic for RegistroV.xaml
+    /// </summary>
+    public partial class RegistroV : Window
+    {
+        public RegistroV()
+        {
+            InitializeComponent();
+        }
+    }
+}
